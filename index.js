5f4e8d6dd94624151163679b4f56cc82ce65214f
ahmedjw <ahmedvander@gmail.com>
2025-07-23 16:26:40 +0300

    inital vaersion of main file

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // This is important for enabling Node.js integration
+    },
+  });
+  win.loadFile("index.html");
+  win.webContents.openDevTools(); // Open DevTools for debugging
+};
+app.on("ready", () => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
